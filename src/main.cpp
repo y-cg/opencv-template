77260a98a0eb0acdce4311ff19c14229079d880f
YCG <devin.cg.yeung@gmail.com>
2024-02-22 19:42:24 +0800

    feat: opencv mwe

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
 
-int main(int argc, char** argv)
-{
-    cout << "hello world!" << endl;
-    return 0;
+int main(int argc, char **argv) {
+  cv::Mat image(3, 3, CV_8UC1);
+  std::cout << CV_VERSION << std::endl;
 }
